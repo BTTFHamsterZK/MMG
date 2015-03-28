ef495974e784563719115fbf925d5bd6a030186a
BTTFHamsterZK <bttfhamsterzk@gmail.com>
2015-03-28 10:12:44 -0400

    Update TPS.java

--- me/BTTFHamster/MMG/Core/TPS.java
+++ me/BTTFHamster/MMG/Core/TPS.java
@@ -1,18 +1,15 @@
 package me.BTTFHamster.MMG.Core;
-public class TPS
-  implements Runnable
-{
+public class TPS  implements Runnable {
+  
   public static int TICK_COUNT= 0;
   public static long[] TICKS= new long[600];
   public static long LAST_TICK= 0L;
  
-  public static double getTPS()
-  {
+  public static double getTPS() {
     return getTPS(100);
   }
  
-  public static double getTPS(int ticks)
-  {
+  public static double getTPS(int ticks) {
     if (TICK_COUNT< ticks) {
       return 20.0D;
     }
@@ -22,18 +19,15 @@ public class TPS
     return ticks / (elapsed / 1000.0D);
   }
  
-  public static long getElapsed(int tickID)
-  {
-    if (TICK_COUNT- tickID >= TICKS.length)
-    {
+  public static long getElapsed(int tickID) {
+    if (TICK_COUNT- tickID >= TICKS.length){
     }
  
     long time = TICKS[(tickID % TICKS.length)];
     return System.currentTimeMillis() - time;
   }
  
-  public void run()
-  {
+  public void run() {
     TICKS[(TICK_COUNT% TICKS.length)] = System.currentTimeMillis();
  
     TICK_COUNT+= 1;
